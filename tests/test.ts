187abb8b4720c4a8528cfe476c05330f8c7f575d
sumoduduk <sumoduduk13@gmail.com>
2024-07-25 14:52:09 +0700

    unit test for filter audio

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,21 @@
 import { expect } from 'chai';
+import fs from 'fs';
+import { videoFormat } from '../src/interfacce';
+import { filterAudio } from '../src/filter_audio';
 
-describe('cheerio function', () => {
-    it('pass the test', () => {
-        expect(true).to.eq(true);
+function loadVideoFormat() {
+    let data = fs.readFileSync('assets/videoFormat.json', 'utf8');
+    return JSON.parse(data) as Array<videoFormat>;
+}
+
+describe('filtter audio test', () => {
+    it('will filter fot the best audio', () => {
+        let formats = loadVideoFormat();
+        let format = filterAudio(formats);
+
+        console.log(format);
+
+        expect(format?.hasAudio).to.eq(true);
+        expect(format?.hasVideo).to.eq(false);
     });
 });
