e2ce1592d69e98e58e65f3e400a4bee216bc5ca2
sumoduduk <sumoduduk13@gmail.com>
2024-07-25 15:33:28 +0700

    chore: change function name to getInfoFOrmats and return valu to array of video format

--- src/get_video_info.ts
+++ src/get_video_info.ts
@@ -1,7 +1,7 @@
 import ytdl from '@distube/ytdl-core';
 import { getRandomIPv6 } from '@distube/ytdl-core/lib/utils';
 
-export async function getInfo(link: string) {
+export async function getInfoFormats(link: string) {
     try {
         const agentForAnotherRandomIP = ytdl.createAgent(undefined, {
             localAddress: getRandomIPv6('2001:2::/48'),
@@ -10,7 +10,7 @@ export async function getInfo(link: string) {
         let info = await ytdl.getInfo(link, {
             agent: agentForAnotherRandomIP,
         });
-        console.log(info.formats);
+        return info.formats;
     } catch (error) {
         console.error(error);
     }
