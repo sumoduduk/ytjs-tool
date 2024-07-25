cb23ad59caf7af6447294d21aa8cf92a67a91dae
sumoduduk <sumoduduk13@gmail.com>
2024-07-25 17:58:10 +0700

    feat: add function to check file if exist

--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,19 @@ export function loadVideoFormat() {
     let data = fs.readFileSync('assets/videoFormat.json', 'utf8');
     return JSON.parse(data) as Array<videoFormat>;
 }
+
+export function checkIfFileExists(filePath: string) {
+    try {
+        fs.accessSync(filePath, fs.constants.F_OK);
+        console.log('File exists');
+        return true;
+    } catch (err: any) {
+        if (err.code === 'ENOENT') {
+            console.log('File does not exist');
+            console.log('Downloading audio...');
+            return false;
+        } else {
+            throw err;
+        }
+    }
+}
