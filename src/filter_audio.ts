73144b53198dd9aa895e19346f9a0d000cc97d37
sumoduduk <sumoduduk13@gmail.com>
2024-07-25 14:53:08 +0700

    fix: filter for besy audio quality

--- src/filter_audio.ts
+++ src/filter_audio.ts
@@ -26,4 +26,23 @@ const sortFormatsBy = (
 const sortFormatsByAudio = (a: videoFormat, b: videoFormat) =>
     sortFormatsBy(a, b, [getAudioBitrate, getAudioEncodingRank]);
 
-function filterAudio(formats: Array<videoFormat>) {}
+const filterFormat = (formats: Array<videoFormat>) => {
+    return formats.filter(
+        (format) => !!format.url && !format.hasVideo && format.hasAudio,
+    );
+};
+
+export function filterAudio(formats: Array<videoFormat>) {
+    formats = filterFormat(formats);
+    formats.sort(sortFormatsByAudio);
+    const bestFormat = formats[0];
+    formats = formats.filter((f) => sortFormatsByAudio(bestFormat, f));
+
+    const worstVideoQuality = formats
+        .map((f) => parseInt(f.qualityLabel) || 0)
+        .sort((a, b) => a - b)[0];
+    let format = formats.find(
+        (f) => (parseInt(f.qualityLabel) || 0) === worstVideoQuality,
+    );
+    return format;
+}
