791b5981fcd81d406b15d7b049abd38aecd3ecf4
sumoduduk <sumoduduk13@gmail.com>
2024-07-26 20:18:24 +0700

    add random ip proxy

--- src/download_plain.ts
+++ src/download_plain.ts
@@ -1,8 +1,28 @@
 import { Downloader } from 'nodejs-file-downloader';
 
+import { get_randIP } from './get_rand';
+
 export async function downloadPlain(url: string) {
     const downloader = new Downloader({
         url: url,
+        directory: './music',
+    });
+    try {
+        const report = await downloader.download();
+        console.log('Download Complete', report);
+        return report;
+    } catch (error) {
+        console.log('Download plain error', error);
+    }
+}
+
+async function downloadWithProxy(url: string) {
+    let ip = await get_randIP();
+    if (!ip) return console.log('error rand ip');
+
+    const downloader = new Downloader({
+        url: url,
+        proxy: ip,
         directory: './music',
     });
     try {
