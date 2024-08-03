6b8477d2981310f89400176d988e65c92c5ea62c
sumoduduk <sumoduduk13@gmail.com>
2024-08-03 18:33:05 +0700

    chore: change download folder path

--- src/download_plain.ts
+++ src/download_plain.ts
@@ -1,32 +1,13 @@
 import { Downloader } from 'nodejs-file-downloader';
-
-import { get_randIP } from './get_rand';
+import { DOWNLOAD_PATH } from '..';
 
 export async function downloadPlain(url: string, id: string) {
     let fileName = id + '.mp4';
     const downloader = new Downloader({
         url: url,
-        directory: './music',
+        directory: DOWNLOAD_PATH,
         skipExistingFileName: true,
         fileName,
-    });
-    try {
-        const report = await downloader.download();
-        console.log('Download Complete', report);
-        return report;
-    } catch (error) {
-        console.log('Download plain error', error);
-    }
-}
-
-async function downloadWithProxy(url: string) {
-    let ip = await get_randIP();
-    if (!ip) return console.log('error rand ip');
-
-    const downloader = new Downloader({
-        url: url,
-        proxy: ip,
-        directory: './music',
     });
     try {
         const report = await downloader.download();
