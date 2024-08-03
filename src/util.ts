073ca2cf7bf727f23e101f67b18d40fec1f4b078
sumoduduk <sumoduduk13@gmail.com>
2024-08-03 18:33:46 +0700

    feat: function to get outfile mp3

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { videoFormat } from './interfacce';
+import path from 'path';
 
 export function loadVideoFormat() {
     let data = fs.readFileSync('assets/videoFormat.json', 'utf8');
@@ -20,6 +21,19 @@ export function checkIfFileExists(filePath: string) {
             throw err;
         }
     }
+}
+
+export function getInOutFileMp3(path_file: string) {
+    let parent = path.dirname(path_file);
+    let stem_name = path.basename(path_file, path.extname(path_file));
+
+    let file_name = stem_name + '.mp3';
+    let out_file = path.join(parent, file_name);
+
+    return {
+        in_file: path_file,
+        out_file,
+    };
 }
 
 export function wait(ms: number) {
