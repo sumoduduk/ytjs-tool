881979b47bd08a1ba117b33dff7d610588c18944
sumoduduk <sumoduduk13@gmail.com>
2024-08-03 18:32:42 +0700

    fix: fix function to convert mp4 to mp4

--- src/convert.ts
+++ src/convert.ts
@@ -1,23 +1,47 @@
-import { promisify } from 'util';
-import { exec } from 'child_process';
-import { promises as fs } from 'fs';
+import { spawn, ChildProcess } from 'child_process';
 
-const asyncExec = promisify(exec);
-
-export async function convertCodec(
-    videoId: string,
-): Promise<{ stdout: string; stderr: string }> {
-    const inFile: string = `music/${videoId}.mp4`;
-    const outFile: string = `music/${videoId}.mp3`;
-
+export function convertCodec(
+    inFile: string,
+    outFile: string,
+): Promise<boolean> {
     const cmdDest: string =
         process.platform === 'win32' ? '.\\data\\ffmpeg' : 'ffmpeg';
 
-    const { stdout, stderr } = await asyncExec(
-        `${cmdDest} -i ${inFile} -vn -ar 44100 -ac 2 -b:a 192k ${outFile}`,
-    );
+    return new Promise((resolve, reject) => {
+        const ffmpeg: ChildProcess = spawn(cmdDest, [
+            '-i',
+            inFile,
+            '-vn',
+            '-ar',
+            '44100',
+            '-ac',
+            '2',
+            '-b:a',
+            '192k',
+            outFile,
+        ]);
 
-    // await fs.unlink(inFile);
+        const args_spawn = ffmpeg.spawnargs.join(' ');
+        console.log('spawn args : ', args_spawn);
 
-    return { stdout, stderr };
+        ffmpeg.stdout?.on('data', (data) => {
+            console.log(`stdout: success`);
+        });
+
+        ffmpeg.stderr?.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        ffmpeg.on('close', (code: number) => {
+            if (code === 0) {
+                resolve(true);
+            } else {
+                console.log('Process failed with exit code : ', code);
+                reject(false);
+            }
+        });
+    });
 }
+
+//spawn args ffmpeg -i assets/test.mp4 -vn -ar 4410 -ac 2 -b:a 192k assets/test.mp3
+// "-i", in_file, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", out_file,
